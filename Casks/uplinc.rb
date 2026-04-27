face9e7f1299fbb90151b8444c832e3566c78507
nantokaworks <ichi0g0y@gmail.com>
2026-04-27 22:14:23 +0900

    uplinc 0.1.6

--- Casks/uplinc.rb
+++ Casks/uplinc.rb
@@ -1,6 +1,6 @@
 cask "uplinc" do
-  version "0.1.5"
-  sha256 "39436a94fc9d439afe3c1c5b360fe7c9a25fc20f2d20d9fa20a46fc33b63277a"
+  version "0.1.6"
+  sha256 "ab8f1de23e0d7451350fcdf38779a5b263cdfdd342ee9a023fd96b9cf1b1140e"
 
   url "https://github.com/ichi0g0y/UplinC/releases/download/v#{version}/UplinC-#{version}.zip"
   name "UplinC"
