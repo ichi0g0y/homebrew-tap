f561bb034b989182c21506674d8cc5746305aace
ICH <ichi0g0y@gmail.com>
2026-04-28 17:09:16 +0900

    🔖 [cask] uplinc 0.1.10 (#2)

--- Casks/uplinc.rb
+++ Casks/uplinc.rb
@@ -1,6 +1,6 @@
 cask "uplinc" do
-  version "0.1.9"
-  sha256 "4c1c3b2bfbe591d734de2b7bc7b9542ea71c25e7786147ad5ab6f34a5cb75e03"
+  version "0.1.10"
+  sha256 "21e290b2b1380c4713910b4abacaea3f395b636b339567f1a5bf5fcb1f144bd0"
 
   url "https://github.com/ichi0g0y/UplinC/releases/download/v#{version}/UplinC-#{version}.zip"
   name "UplinC"
